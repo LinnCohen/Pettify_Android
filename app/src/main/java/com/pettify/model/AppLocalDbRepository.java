9795e7ca44834edc4633aa9adb86e778e32a37e2
Or Krichli <or.krichli1@ibm.com>
2021-03-14 23:45:33 +0200

    report

--- app/src/main/java/com/pettify/model/AppLocalDbRepository.java
+++ app/src/main/java/com/pettify/model/AppLocalDbRepository.java
@@ -3,14 +3,15 @@ package com.pettify.model;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.pettify.model.report.ReportDao;
 import com.pettify.model.user.User;
 import com.pettify.model.user.UserDao;
+import com.pettify.model.report.Report;
+import com.pettify.model.report.ReportDao;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Report.class}, version = 1)
 public abstract class AppLocalDbRepository extends RoomDatabase {
-    public abstract UserDao userDao();
     public abstract ReportDao reportDao();
+    public abstract UserDao userDao();
 }
 
 
