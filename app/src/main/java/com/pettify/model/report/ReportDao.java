4848ff8e15dea3c98013f7a22c39fa4d157656c9
Or Krichli <or.krichli1@ibm.com>
2021-03-14 23:06:17 +0200

    report

---
+++ app/src/main/java/com/pettify/model/report/ReportDao.java
@@ -0,0 +1,23 @@
+package com.pettify.model.report;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ReportDao {
+    @Query("select * from Report")
+    //LiveData<List<Report>> getAll();
+    List<Report> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(Report... reports);
+
+    @Delete
+    void delete(Report report);
+
+}
