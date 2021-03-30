bf3b51754b90d13cbc5ca8e62a55190947d8853f
mays <mayshwa@gmail.com>
2021-03-30 18:38:13 +0300

    change navigation issues and stack

--- app/src/main/java/com/pettify/MainActivity.java
+++ app/src/main/java/com/pettify/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity  {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.reportslist_list)
+                R.id.nav_home, R.id.reportslist_list, R.id.nav_login, R.id.nav_register, R.id.create_report, R.id.nav_chat, R.id.account_tab)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
